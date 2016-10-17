02c492b8eb5afdb82867718e4c278704192e92d5
“nerdale” <alexandra.neira@gmail.com>
2016-10-16 21:43:15 -0300

    arreglo indentación script.js

--- js/script.js
+++ js/script.js
@@ -1,40 +1,41 @@
 // #1 Elaborar una función a la  cual le enviaremos tres enteros y muestra  el menos
-function numeroMenor(arreglo){
-	var arreglo=[];
-	var numeroMenor = Math.min.apply(null, arreglo);
+var ar_menor=[];
+function numeroMenor(ar_menor){
+	var numeroMenor = Math.min.apply(null, ar_menor);
 	for (var i = 0; i <3; i++){
-		arreglo.push(prompt("Ingrese un número"));
+		ar_menor.push(prompt("Ingrese un número"));
 	}
-	alert("el número menor entre " + arreglo[0] + ", " + arreglo[1] + " y " + arreglo[2]+ " es " + Math.min.apply(null, arreglo));
+	alert("el número menor entre " + ar_menor[0] + ", " + ar_menor[1] + " y " + ar_menor[2]+ " es " + Math.min.apply(null, ar_menor));
 }
 /* #2 Escribir el codigo de una funcion a la que se pasa como parametro un numero entero 
 y devuelve como resultado un cadena de texto que indica si el numero es par o impar 
 mostrar por pantalla el resultado devuelto por la funcion*/
-function numerosPares(arreglo) {
-	var arreglo = [];
+
+function numerosPares(numeros_pares) {
+	var numeros_pares = [];
 	var primerNum = prompt("Indique el primer numero del arreglo");
 	var ultimoNum = prompt("Indique el ultimo numero del arreglo");
 	for (var i = primerNum; i <= ultimoNum; i++) {
-		if(i % 2=== 0){
-			arreglo.push(i);
+		if((i % 2) === 0){
+			numeros_pares.push(i);
 		}
 	}
-	return alert("Los numeros pares entre " + primerNum + " y " + ultimoNum + " son: " + arreglo);
+	return alert("Los numeros pares entre " + primerNum + " y " + ultimoNum + " son: " + numeros_pares);
 }
 // #3 Realizar una función a la cual le envie tres enteros y los muestre ordenados de menor  a mayor 
-function menorMayorArreglo(arreglo){
-	var arreglo = [];
+var menor_mayor = [];
+function menorMayorArreglo(menor_mayor){
 	var texto = "Los números ordenados de menor a mayor es ";
 	for(var i = 0; i < 3; i++){
-		arreglo.push(prompt("Ingrese un numero"));
+		menor_mayor.push(prompt("Ingrese un numero"));
 	}
 	function comparacion(primero, segundo) {
 		return primero - segundo; //-1 0 1
 	}
-	alert(texto + arreglo.sort(comparacion));
+	alert(texto + menor_mayor.sort(comparacion));
 }
 // #7 Realizar un programa que calcule el cuadrado de la suma de dos numeros
-function sumaCuadrado(arreglo){
+function sumaCuadrado(){
 	var arreglo = [3, 2];
 	var suma = [3, 2].reduce(add, 0);
 	function add(a, b) {
