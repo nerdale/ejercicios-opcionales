3fb2dc243064fe1eda360b744f0d0d5c18f37852
“nerdale” <alexandra.neira@gmail.com>
2016-10-17 22:14:36 -0300

    agregado funciones sin arreglos

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
-// #1 Elaborar una función a la  cual le enviaremos tres enteros y muestra  el menos
 
+// //ARREGLOS
 function numeroMenor(){
 	var ar_menor=[];
 	var numeroMenor = Math.min.apply(null, ar_menor);
 	for (var i = 0; i <3; i++){
 		ar_menor.push(prompt("Ingrese un número"));
 	}
-	return alert("el número menor entre " + ar_menor[0] + ", " + ar_menor[1] + " y " + ar_menor[2]+ " es " + Math.min.apply(null, ar_menor));
+	alert("el número menor entre " + ar_menor[0] + ", " + ar_menor[1] + " y " + ar_menor[2]+ " es " + Math.min.apply(null, ar_menor));
 }
-/* #2 Escribir el codigo de una funcion a la que se pasa como parametro un numero entero 
-y devuelve como resultado un cadena de texto que indica si el numero es par o impar 
-mostrar por pantalla el resultado devuelto por la funcion*/
+
 function numerosPares(){
 	var arreglo_numeros = [];
 	for(var i = 0; i < 5; i++){
@@ -21,10 +19,9 @@ function numerosPares(){
 			return num % 2 === 0;
 		}
 	})
-	return alert("Los números ingresados son: " + arreglo_numeros + " y los números pares son: " + pares);
+	alert("Los números ingresados son: " + arreglo_numeros + " y los números pares son: " + pares);
 }
 
-// #3 Realizar una función a la cual le envie tres enteros y los muestre ordenados de menor  a mayor 
 function menorMayorArreglo(){
 	var menor_mayor = [];
 	var texto = "Los números ordenados de menor a mayor es ";
@@ -34,23 +31,108 @@ function menorMayorArreglo(){
 	function comparacion(primero, segundo) {
 		return primero - segundo; //-1 0 1
 	}
-	return alert(texto + menor_mayor.sort(comparacion));
+	alert(texto + menor_mayor.sort(comparacion));
 }
-// #7 Realizar un programa que calcule el cuadrado de la suma de dos numeros
+
 function sumaCuadrado(){
 	var arreglo = [3, 2];
 	var suma = [3, 2].reduce(suma, 0);
 	function suma(elementoUno, elementoDos) {
 		return elementoUno + elementoDos;
 	}
-	return alert("El resultado de la sumatoria entre los números  " + arreglo[0] + " y " + arreglo[1] + " es " + suma + " y el cuadrado del resultado es " + Math.pow(suma,2));
+	alert("El resultado de la sumatoria entre los números  " + arreglo[0] + " y " + arreglo[1] + " es " + suma + " y el cuadrado del resultado es " + Math.pow(suma,2));
 }
 
-function sumaCuadrado(){
+/*function sumaCuadrado(){
 	var numeros = [3,2];
 	var sum = 0;
 	for(var i = 0; i < numeros.length; i++){
 		sum += numeros[i]
 	}
-	return alert("La suma entre los números " + numeros[0] + " y " + numeros[1] + " es: " + sum +  " y el cuadrado del resultado es " + Math.pow(sum,2));
+	alert("La suma entre los números " + numeros[0] + " y " + numeros[1] + " es: " + sum +  " y el cuadrado del resultado es " + Math.pow(sum,2));
+}*/
+
+//FUNCIONES
+
+function numeroMenorFuncion(numUno, numDos, numTres){
+	var numUno = parseInt(prompt("Ingrese el primer numero"));
+	var numDos = parseInt(prompt("Ingrese el segundo numero"));
+	var numTres = parseInt(prompt("Ingrese el tercer numero"));
+	var numeroMenor = Math.min(numUno, numDos, numTres);
+	alert("El número menor entre : " + numUno + "," + numDos + "," + numTres + " es: " + numeroMenor);
 }
+
+function esParFuncion(num){
+	var num = parseInt(prompt("Ingrese un número"));
+	var resultado = "El número " + num + " ";
+	if (num % 2 === 0){
+		resultado += "es par";
+	} else {
+		resultado += "es impar";		
+	}
+	alert(resultado);
+}
+
+function menorMayorFuncion(a, b, c){
+	var a = parseInt(prompt("Ingrese primer número"));
+	var b = parseInt(prompt("Ingrese segundo número"));
+	var c = parseInt(prompt("Ingrese tercer número"));
+	var resultado = "El orden de menor a mayor ";
+	if(a < b && a < c && b < c){
+		return resultado += "es: " + a + ", " + b +  ", " + c;
+	}else if (a < b && a < c && b > c){
+		return resultado += "es: " + a +  ", " + c +  ", " + b;
+	}else if (b < a && b < c && c < a){
+		return resultado += "es: " + b +  ", " + c +  ", " + a;
+	}else if (c < a && c < b && a > b){
+		return resultado += "es: " + c +  ", " + b +  ", " + a;
+	}else if ( c < b && c < a && b > a){
+		return resultado += "es: " + c +  ", " + a +  ", " + b;
+	}
+	alert(resultado);
+}
+
+function minMayFuncion(palabra){
+	var palabra = prompt("Ingrese la palabra que quiere verificar");
+	var resultado ="El texto " + palabra + " ";
+	if (palabra === palabra.toUpperCase()){
+		resultado += "está compuesto por mayúsculas";
+	} else if (palabra === palabra.toLowerCase()){
+		resultado += "está compuesto por minúsculas";
+	} else {
+		resultado += "está compuesto por mayúsculas y minúsculas";
+	}
+	alert(resultado);
+}
+
+function sumaCuadradoFuncion(numUno, numDos){
+	var numUno = parseInt(prompt("Ingrese el primer numero"));
+	var numDos = parseInt(prompt("Ingrese el segundo numero"));
+	var resultado = (numUno + numDos);
+	alert("El resultado de la sumatoria entre " + numUno + " y " + numDos + " es: " + resultado + " y el cuadrado de este es " + Math.pow(resultado, 2));
+}
+
+function digitosFuncion(numero){
+	var numero = parseInt(prompt("Ingrese un digito"));
+	var digitoString = numero.toString();
+	var cantidadDigitos = 0;	
+	for (var i = 0; i < digitoString.length; ++i){
+		++cantidadDigitos;
+	}
+	alert( "La cantidad de digitos es: " + cantidadDigitos);
+}
+
+function palindromo(palabra){
+	var palabra = prompt("ingrese una palabra");
+	var resultado = "La palabra ingresada ";
+	var especiales = /[^A-Za-z0-9]/g;
+	var palabra = palabra.toLowerCase().replace(especiales, "");
+	var largoP = palabra.length;
+	for (var i = 0; i < largoP/2; ++i){
+		if(palabra[i] !== palabra[largoP - 1 - i]){
+			return resultado += "no es palíndromo";
+		}
+	}
+	return resultado += "es palíndromo";
+}
+
